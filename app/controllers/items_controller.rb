3cb70f4fa0c34149bcc1629dd3a387d3cb2c8b25
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-29 00:50:15 +0300

    complted assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end 
+
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    if item
+      render json: item, include: :user
+    else
+      render json: "Item not found", status: 404
+    end
+  end
+
+def create
+  item = Item.create!(params_item)
+  if item
+    render json: item, status: :created
+  else
+    render json: item, status: :not_found
+  end
+end
+private
+
+def params_item
+  params.permit(:name, :description, :price, :user_id)
+end
+
+def render_not_found_response
+  render json: { error: "Item not found" }, status: :not_found
+end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
+  def index
+  users = User.all
+  if users
+  render json: users, status: :ok
+  else
+  render json: { message: "No users found" }, status: :not_found
+  end
+  end
 
   def show
-    user = User.find_by(id: params[:id])
+    if params[:item_id]
+    user = User.find(params[:item_id])
+    else
+    user = User.find(params[:id])
+    end
     render json: user, include: :items
   end
+
+ 
 
 end
