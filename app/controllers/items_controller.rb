619cd4e1d95a435e344c31d30ac2a5333cf1cb70
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-29 00:53:48 +0300

    commit tree 81ceba35b8b26825bb3ebd2e6c714f71c133c676

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,41 +1,36 @@
 class ItemsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
     if params[:user_id]
       user = User.find(params[:user_id])
       items = user.items
     else
       items = Item.all
-    end 
+    end
 
     render json: items, include: :user
   end
 
   def show
     item = Item.find(params[:id])
-    if item
-      render json: item, include: :user
-    else
-      render json: "Item not found", status: 404
-    end
+
+    render json: item, include: :user
   end
 
-def create
-  item = Item.create!(params_item)
-  if item
+  def create
+    item = Item.create!(params_item)
+
     render json: item, status: :created
-  else
-    render json: item, status: :not_found
   end
-end
-private
 
-def params_item
-  params.permit(:name, :description, :price, :user_id)
-end
+  private
 
-def render_not_found_response
-  render json: { error: "Item not found" }, status: :not_found
-end
+  def params_item
+    params.permit(:name, :description, :price, :user_id)
+  end
 
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
 end
